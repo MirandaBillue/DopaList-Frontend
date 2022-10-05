fbb85d22ea740ef4fc9cdeb770849c9e90eee1d0
Miranda Billue <110904846+MirandaBillue@users.noreply.github.com>
2022-10-05 13:03:52 -0400

    HomePage Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,54 @@
 import { login, logout } from "../services/firebase";
+import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
 function Header(props) {
   return (
-      <Navbar expand="lg" variant="dark" bg="dark">
-        <Container>
-      <Navbar.Brand href="/">DopaList</Navbar.Brand> 
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        {
-          props.user ?
-            <>
-            <div style={{ marginRight: 10, color:"white"}}>Welcome, {props.user.displayName}</div>
-            <img src={props.user.photoURL} alt="" style={{
-              height: 30,
-              width: 30,
-              borderRadius: '50%',
-              marginRight: 10
-             }}
-              />     
+    <Navbar expand="lg" variant="dark" bg="dark">
+      <Container>
+        <Navbar.Brand href="/">DopaList</Navbar.Brand>
+        <Link to="/todo">
+          <div style={{ color: 'white' }}>Todo Lists</div>
+        </Link>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          {
+            props.user ?
+              <>
+                <div style={{ marginRight: 10, color: "white" }}>Welcome, {props.user.displayName}</div>
+                <img src={props.user.photoURL} alt="" style={{
+                  height: 30,
+                  width: 30,
+                  borderRadius: '50%',
+                  marginRight: 10
+                }}
+                />
+                <div
+                  onClick={logout}
+                  style={{
+                    cursor: 'pointer',
+                    marginRight: 10,
+                    color: 'white'
+                  }}>
+                  Logout
+                </div>
+              </>
+              :
               <div
-                onClick={logout}
+                onClick={login}
                 style={{
                   cursor: 'pointer',
                   marginRight: 10,
-                  color:'white'
+                  color: "white"
                 }}>
-                Logout
+                Login
               </div>
-            </>
-            :
-            <div
-              onClick={login}
-              style={{
-                cursor: 'pointer',
-                marginRight: 10,
-                color:"white"
-              }}>
-              Login
-            </div>
-        }
-      </div>
- </Container>
- </Navbar>
+          }
+        </div>
+      </Container>
+    </Navbar>
 
-);
+  );
 }
 
 export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+      <div className="home">
+<h1>DopaList</h1>
+      </div>
+    ) 
+  }
+  
+  export default Home;
